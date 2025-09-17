b555f451125df97ef8e349547178707bfb4f0887
Ryan Parag <parag.ryan@gmail.com>
2025-09-17 08:26:01 -0400

    add max rounds

--- src/components/GameSetup.tsx
+++ src/components/GameSetup.tsx
@@ -81,7 +81,7 @@ export const GameSetup: React.FC<GameSetupProps> = ({ onBack, onNext }) => {
                 Number of Rounds
               </label>
               <div className="flex flex-wrap items-center justify-center">
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((rounds) => (
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((rounds) => (
                   <div
                     key={rounds}
                     className="inline-flex p-2"
