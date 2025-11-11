397d54e25e46a36e1d68a6a7954ddbd5a7c6b4e6
Ryan Parag <parag.ryan@gmail.com>
2025-11-11 09:03:09 -0500

    og image

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import ogPlugin from 'vite-plugin-open-graph';
+import type { Options } from 'vite-plugin-open-graph';
+
+const ogURL = 'https://scorekeeper.ryanparag.coml';
+const ogImage = '/src/public/images/og-image.png';
+const ogTitle = 'ScoreKeeper by Ryan Parag';
+const ogDescription = 'Add players and track scores across all your favorite card games';
+
+const ogOptions: Options = {
+  basic: {
+    url: ogURL,
+    title: ogTitle,
+    type: 'image.png',
+    image: ogImage,
+    determiner: 'auto',
+    description: ogDescription,
+    siteName: ogTitle,
+  },
+  twitter: {
+    image: ogImage
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ogPlugin(ogOptions)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
