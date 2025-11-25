1c179b6a0e5183510f5694e96a35dea8907bfea8
Ryan Parag <parag.ryan@gmail.com>
2025-11-25 12:03:24 -0500

    about section

--- src/components/GameSetup.tsx
+++ src/components/GameSetup.tsx
@@ -121,7 +121,12 @@ export const GameSetup: React.FC<GameSetupProps> = ({ onBack, onNext }) => {
                 ))}
               </motion.div>
               {
-                !showMoreRounds && (
+                showMoreRounds ? (
+                  <div className="mt-4 p-2 rounded-lg bg-stone-100 dark:bg-stone-700 flex justify-center text-center text-sm text-stone-500 dark:text-stone-400">
+                    <span>😎 Don't worry, you can increase/decrease the number of rounds (without limit) while playing too</span>
+                  </div>
+                ) :
+                (
                   <div className="mt-2 flex justify-center">
                     <Button
                         variant="text"
---
+++ src/components/ui/tag.tsx
@@ -0,0 +1,15 @@
+export const TagLink = ({ href, children, withIcon }) => {
+    return(
+        <a href={href} target="_blank" className={`transition transform relative translate-y-1 inline-flex items-center py-0.5 ${withIcon ? 'pl-0.5 pr-1.5' : 'px-1.5'} rounded-lg border border-stone-300 dark:border-stone-700 text-sm bg-gradient-to-b from-white to-stone-200 dark:from-stone-800 dark:to-stone-700 hover:border-stone-400 dark:hover:border-stone-600 text-stone-950 dark:text-white font-medium shadow-none hover:shadow-sm`}>
+            {children}
+        </a>
+    )
+}
+
+export const Tag = ({ children, withIcon }) => {
+    return(
+        <span className={`transition transform relative translate-y-1 inline-flex items-center ${withIcon ? 'pl-0.5 pr-1.5' : 'px-1.5'} rounded-lg border border-stone-300 dark:border-stone-700 text-sm bg-gradient-to-b from-white to-stone-200 dark:from-stone-800 dark:to-stone-700 text-stone-950 dark:text-white font-medium`}>
+            {children}
+        </span>
+    )
+}
