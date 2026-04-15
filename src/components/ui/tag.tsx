7ee1bef739b8c606dd67e640e3b00e4afa911d0e
Ryan Parag <parag.ryan@gmail.com>
2026-04-15 10:38:25 -0400

    hover shims for tag links

--- src/components/ui/tag.tsx
+++ src/components/ui/tag.tsx
@@ -1,14 +1,17 @@
+import HoverShim from './HoverShim';
+
 export const TagLink = ({ href, children, withIcon }) => {
     return(
-        <a href={href} target="_blank" className={`transition transform relative translate-y-0.5 inline-flex items-center py-0.5 ${withIcon ? 'pl-1 pr-1.5' : 'px-1.5'} rounded-lg border border-border text-sm bg-gradient-to-b from-background hover:to-background to-secondary text-foreground font-medium shadow-none hover:shadow-sm transform active:scale-[97%] active:shadow-inner`}>
+        <a href={href} target="_blank" className={`transition transform relative translate-y-0.5 inline-flex items-center border border-black/20 dark:border-white/20  py-0.5 group ${withIcon ? 'pl-1 pr-1.5' : 'px-1.5'} rounded-lg text-sm bg-secondary text-foreground font-medium shadow-none hover:shadow-sm transform active:scale-[97%] active:shadow-inner`}>
             {children}
+            <HoverShim/>
         </a>
     )
 }
 
 export const Tag = ({ children, withIcon }) => {
     return(
-        <span className={`transition transform relative translate-y-0.5 inline-flex items-center ${withIcon ? 'pl-1 pr-1.5' : 'px-1.5'} rounded-lg border border-border text-sm bg-gradient-to-b from-secondary to-muted text-foreground font-medium`}>
+        <span className={`transition transform relative translate-y-0.5 inline-flex items-center border border-black/20 dark:border-white/20 ${withIcon ? 'pl-1 pr-1.5' : 'px-1.5'} rounded-lg text-sm bg-gradient-to-b from-secondary to-muted text-foreground font-medium`}>
             {children}
         </span>
     )
